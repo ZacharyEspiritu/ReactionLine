d79e6fd87ccda016574c05da20e3580841fc3ae5
Zachary Espiritu <zespiritu17@regis.org>
2015-07-19 11:35:54 -0400

    Added some documentation

--- ReactionLine.spritebuilder/SharingHandler.swift
+++ ReactionLine.spritebuilder/SharingHandler.swift
@@ -3,7 +3,7 @@
 //  ReactionLine
 //
 //  Created by Zachary Espiritu on 7/19/15.
-//  Copyright (c) 2015 Apportable. All rights reserved.
+//  Copyright (c) 2015 Zachary Espiritu. All rights reserved.
 //
 
 import UIKit
@@ -11,7 +11,8 @@ import Social
 
 class SharingHandler: UIViewController {
     
-    // Making the SharingHandler a singleton:
+    // MARK: Singleton
+    
     class var sharedInstance : SharingHandler {
         struct Static {
             static let instance : SharingHandler = SharingHandler()
@@ -19,20 +20,30 @@ class SharingHandler: UIViewController {
         return Static.instance
     }
     
+    
+    // MARK: Sharing Functions
+    
+    /**
+    Opens the native iOS share screen for posting to Twitter.
+    */
     func postToTwitter() {
         if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
             var twitterViewController: SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
-            twitterViewController.setInitialText("Test tweet!")
+            twitterViewController.setInitialText("Testing twitter integration!")
             CCDirector.sharedDirector().presentViewController(twitterViewController, animated: true, completion: nil)
         }
     }
     
+    /**
+    Opens the native iOS share screen for posting to Facebook.
+    
+    **Note:** A known caveat with the native iOS "share sheet" for Facebook is that you are no longer able to "pre-fill" the content of the box. If you want to be able to do this, you'll need to integrate the Facebook SDK, which is an entirely different ballgame of its own.
+    */
     func postToFacebook() {
         if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
             var facebookViewController: SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
-            facebookViewController.setInitialText("Test")
+            // This does not work; see above: facebookViewController.setInitialText("Testing facebook integration!")
             CCDirector.sharedDirector().presentViewController(facebookViewController, animated: true, completion: nil)
         }
     }
-    
 }
